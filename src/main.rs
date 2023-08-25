0c086cd57e508a7e58997121e821427ab99ce38d
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-08-25 19:53:54 +0700

    Add map drawing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 pub mod timer;
 use logger;
 use gfx;
-use std::thread;
 
 fn say_hi() {
     logger::log(
@@ -22,7 +21,7 @@ fn main() {
 }
 
 const TILE_SIZE: usize = 8;
-const TEST_TILE: [u32; TILE_SIZE * TILE_SIZE] = [
+const TILE_ONE: [u32; TILE_SIZE * TILE_SIZE] = [
     0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
     0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
     0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
@@ -32,6 +31,44 @@ const TEST_TILE: [u32; TILE_SIZE * TILE_SIZE] = [
     0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
     0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_FF_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
 ];
+const TILE_TWO: [u32; TILE_SIZE * TILE_SIZE] = [
+    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
+    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
+    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
+    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_FF, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_FF_00_FF, 0xFF_00_FF_00,
+    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_FF_00_FF, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
+    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
+    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
+    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_FF_00_FF, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
+];
+const tiles: [[u32; TILE_SIZE * TILE_SIZE]; 2] = [
+    TILE_ONE,
+    TILE_TWO,
+];
+const map_size_x: usize = 30;
+const map_size_y: usize = 20;
+const map: [u32; map_size_y * map_size_x] = [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
 
 fn run() {
     let mut window = gfx::Screen::new(
@@ -39,7 +76,7 @@ fn run() {
         240,
         5,
         "BioSphere",
-        30,
+        33,
     );
 
     let mut timer_main = timer::Timer::new();
@@ -47,11 +84,17 @@ fn run() {
         timer_main.start();
 
         window.draw_rectangle(0, 0, 160, 240, 0xFF_181818);
-        window.draw_sprite(&TEST_TILE, TILE_SIZE, TILE_SIZE, 10, 12);
+        // window.draw_sprite(&TILE_ONE, TILE_SIZE, TILE_SIZE, 10, 12);
+        for y in 0..map_size_y {
+            for x in 0..map_size_x {
+                let tile = map[y * map_size_x + x];
+                window.draw_sprite(&tiles[tile as usize], TILE_SIZE, TILE_SIZE, y * TILE_SIZE, x * TILE_SIZE);
+            }
+        }
         window.redraw();
 
         timer_main.stop();
-        window.add_to_title(format!(" - {}\\{}ms",
+        window.add_to_title(format!("{}\\{}ms",
             timer_main.get_time_between_as_micros(),
             window.max_update_time_as_micros,
         ));
