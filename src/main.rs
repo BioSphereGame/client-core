3848668e6db82090c9d7236caa28c48dc8334265
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2023-08-26 21:21:02 +0700

    Use text

--- src/main.rs
+++ src/main.rs
@@ -22,30 +22,8 @@ fn main() {
     run();
 }
 
-const SCALE: usize = 4;
-const TILE_SIZE: u16 = 8;
-const TILE_ONE_DATA: [u32; TILE_SIZE as usize * TILE_SIZE as usize] = [
-    0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
-    0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
-    0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
-    0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_FF_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_FF_00_FF, 0xFF_00_00_FF,
-    0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_FF_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
-    0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
-    0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
-    0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_FF_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF, 0xFF_00_00_FF,
-];
-const TILE_TWO_DATA: [u32; TILE_SIZE as usize * TILE_SIZE as usize] = [
-    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
-    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
-    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
-    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_FF, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_FF_00_FF, 0xFF_00_FF_00,
-    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_FF_00_FF, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
-    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
-    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
-    0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_FF_00_FF, 0xFF_00_FF_00, 0xFF_00_FF_00, 0xFF_00_FF_00,
-];
-
 fn run() {
+    const SCALE: usize = 1;
     let mut window = gfx::Screen::new(
         720 / SCALE,
         1280 / SCALE,
@@ -59,25 +37,25 @@ fn run() {
             false,
             false,
         ),
+        include_bytes!("../assets/UbuntuMono.ttf").to_vec(),
     );
-
-    let tiles = vec![
-        tile::tile_from_1d_data(TILE_ONE_DATA.to_vec(), TILE_SIZE, TILE_SIZE),
-        tile::tile_from_1d_data(TILE_TWO_DATA.to_vec(), TILE_SIZE, TILE_SIZE),
-    ];
-
-    let mut map = map::MapData::new(
-        (720.0 / SCALE as f64 / TILE_SIZE as f64).ceil() as u16,
-        (1280.0 / SCALE as f64 / TILE_SIZE as f64).ceil() as u16,
-    );
-    map.set_tiles(tiles);
-    map.randomize();
+    let size_y = 720 / SCALE;
+    let size_x = 1280 / SCALE;
 
     let mut timer_main = timer::Timer::new();
     while window.is_open() {
         timer_main.start();
 
-        map::draw_map(&mut window, &map, 0, 0, 720 / 4, 1280 / 4);
+        window.draw_rectangle(0, 0, size_y, size_x, 0xFF_000000);
+        window.draw_text(
+            0,
+            0,
+            (50.0 / (SCALE as f64 / 1.5)) as usize,
+            0xFF_FFFFFF,
+            0xFF_AAAAAA,
+            "AaBbCcDdEeFfGg!@#$%^&*()_+",
+        );
+
         window.redraw();
 
         timer_main.stop();
